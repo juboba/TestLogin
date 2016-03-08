439d6e17ef5c6a128967c08300b24fef40db6435
Julio Borja Barra <juboba@gmail.com>
2016-03-08 11:39:53 +0000

    Add build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -5,6 +5,9 @@ var uglify = require('gulp-uglify');
 module.exports = function(gulp, config){
 
     gulp.task('build', function(){
+        return gulp.src(config.paths.sources.js)
+            .pipe(uglify())
+            .pipe(gulp.dest(config.paths.dist));
     });
 
 };
--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,10 @@ var config = require('./gulp/config');
 
 require('./gulp/tasks/browser-sync')(gulp, config);
 require('./gulp/tasks/stubby')(gulp, config);
+require('./gulp/tasks/build')(gulp, config);
 require('./gulp/tasks/test')(gulp, config);
 
-gulp.task('default', ['stubby', 'browserSync', 'watch'], function(){
+//TODO: put these in order with runSequence:
+gulp.task('default', ['stubby', 'build', 'browserSync', 'watch'], function(){
     console.log('hello!');
 });
