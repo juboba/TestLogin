61a6c399bd58cc5807ab1f0aaf3798070f23776b
Julio Borja Barra <jaborja@atsistemas.com>
2016-03-10 12:38:52 +0100

    Fix controller test.

--- test/unit/controllers/login-controller.spec.js
+++ test/unit/controllers/login-controller.spec.js
@@ -1,72 +1,26 @@
 describe('Login tests', function(){
     beforeEach(module('testApp'));
 
-    var Login, $rootScope;
+    var Login, $rootScope, $controller;
 
-    beforeEach(inject(function(_Login_, _$rootScope_){
+    beforeEach(inject(function(_Login_, _$rootScope_, _$controller_){
         Login = _Login_;
         $rootScope = _$rootScope_;
+        $controller = _$controller_;
     }));
 
-    describe('Login tests', function(){
+    it('should call Login->make', function(){
+        var $scope = {};
 
-        it('should not allow a password with less than 8 characters', function(){
-	    var email = 'joe@domain.com';
-	    var password = '1234';
+        spyOn(Login, 'make').and.callThrough();
 
-	    Login.make(email, password)
-		.then(function(res){
-		    //expect(res).toBe(true);
-		}, function(res){
-		    //expect(res).toBe(true);
-		});
+        var loginController = $controller('loginController', {$scope: $scope});
 
-	    $rootScope.$apply();
+        $scope.email = 'joe@domain.com';
+        $scope.password = 'anypassword';
 
-	    expect(false).toBe(true);
+        $scope.login();
 
-        });
-
-	/*
-        it('should not allow login if the email is not well formatted', function(){
-            var $scope = {};
-            var controller = $controller('loginController', {$scope: $scope});
-            var result = false;
-
-            $scope.email = 'myuser';
-            $scope.password = '12345678';
-
-            result = $scope.validate();
-
-            expect(result).toBe(false);
-        });
-
-        it('should not allow the login of password with value "password"', function(){
-            var $scope = {};
-            var controller = $controller('loginController', {$scope: $scope});
-            var result = false;
-
-            $scope.email = 'valid@mail.com';
-            $scope.password = 'password';
-
-            result = $scope.validate();
-
-            expect(result).toBe(false);
-        });
-
-        it('should allow the login of a valid email and password', function(){
-            var $scope = {};
-            var controller = $controller('loginController', {$scope: $scope});
-            var result = false;
-
-            $scope.email = 'valid@mail.com';
-            $scope.password = '12345678';
-
-            result = $scope.validate();
-
-            expect(result).toBe(true);
-        });
-	*/
+        expect(Login.make).toHaveBeenCalled();
     });
-
 });
